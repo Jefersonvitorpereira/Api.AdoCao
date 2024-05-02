2872994a49996a33f8f3b05e378c10eb2900d225
Jheff <79949084+Jefersonvitorpereira@users.noreply.github.com>
2024-05-02 15:18:59 -0300

    Update

---
+++ api.adocao/src/main/java/com/example/api/adocao/SistemaDeAdocaoApiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.api.adocao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeAdocaoApiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeAdocaoApiBackendApplication.class, args);
+	}
+
+}
---
+++ api.adocao/src/test/java/com/example/api/adocao/SistemaDeAdocaoApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api.adocao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeAdocaoApiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
